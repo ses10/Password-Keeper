a593515abec758ade54d42385d9d5406c71d2f33
ses10 <sesma@live.com>
2015-11-10 18:14:39 -0500

    add create table

--- app/src/main/java/com/ses/passwordkeeper/AccountDatabaseHelper.java
+++ app/src/main/java/com/ses/passwordkeeper/AccountDatabaseHelper.java
@@ -16,11 +16,29 @@ public class AccountDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        updateMyDatabase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
 
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
+
+        if(oldVersion < 1){
+            //create the table
+            db.execSQL("CREATE TABLE Account ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "LOGIN TEXT, "
+                    + "PASSWORD TEXT, "
+                    + "QUESTION_1 TEXT, "
+                    + "QUESTION_2 TEXT, "
+                    + "QUESTION_3 TEXT, "
+                    + "NOTES TEXT);");
+
+        }
     }
 }
