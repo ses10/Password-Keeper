c13b451d91f96b0153480099a37802a7827efd8c
ses10 <sesma@live.com>
2015-11-10 18:33:45 -0500

    add insertAccount function

--- app/src/main/java/com/ses/passwordkeeper/AccountDatabaseHelper.java
+++ app/src/main/java/com/ses/passwordkeeper/AccountDatabaseHelper.java
@@ -1,6 +1,7 @@
 package com.ses.passwordkeeper;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -24,6 +25,19 @@ public class AccountDatabaseHelper extends SQLiteOpenHelper {
         updateMyDatabase(db, oldVersion, newVersion);
     }
 
+    private static void insertAccount(SQLiteDatabase db, String name, String login,
+                                      String Password, String QUESTION_1, String QUESTION_2, String QUESTION_3, String notes){
+
+        ContentValues accountValues = new ContentValues();
+        accountValues.put("NAME", name);
+        accountValues.put("LOGIN", login);
+        accountValues.put("PASSWORD", Password);
+        accountValues.put("QUESTION_1", QUESTION_1);
+        accountValues.put("QUESTION_2", QUESTION_2);
+        accountValues.put("QUESTION_3", QUESTION_3);
+        accountValues.put("NOTES", notes);
+        db.insert("Account", null, accountValues);
+    }
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
 
@@ -39,6 +53,9 @@ public class AccountDatabaseHelper extends SQLiteOpenHelper {
                     + "QUESTION_3 TEXT, "
                     + "NOTES TEXT);");
 
+            //insert test rows
+            insertAccount(db,"Gmail", "sesma@gmail.com", "qwert1", "question1","question1","question1","notes");
+            insertAccount(db,"Facebook", "dses10@aol.com", "zewbracakes", "what?","where?","why?","dfsa asdf aefw ads");
         }
     }
 }
