fd10cc59f8410743703251dedf5d3f1b7b4963db
ses10 <sesma@live.com>
2015-11-11 18:39:03 -0500

    add AccountDetailActivity skeleton

---
+++ app/src/main/java/com/ses/passwordkeeper/AccountDetailActivity.java
@@ -0,0 +1,14 @@
+package com.ses.passwordkeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_detail);
+    }
+
+}
