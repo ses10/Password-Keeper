03e509d7685146afa4aba3df87b4487b99786824
ses10 <sesma@live.com>
2015-11-16 18:45:25 -0500

    implement AccountListFragment interface

--- app/src/main/java/com/ses/passwordkeeper/MainActivity.java
+++ app/src/main/java/com/ses/passwordkeeper/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ses.passwordkeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AccountListFragment.AccountListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //implement AccountListListener.itemClicked
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, AccountDetailActivity.class);
+        intent.putExtra(AccountDetailActivity.EXTRA_ACCOUNT_ID, (int)id);
+        startActivity(intent);
+    }
 }
