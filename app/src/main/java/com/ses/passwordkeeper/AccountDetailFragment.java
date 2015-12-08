18bdeab3f34861c43471fceff8a7e3fa86d534d0
ses10 <sesma@live.com>
2015-12-08 17:40:21 -0500

    successful connection to account db

--- app/src/main/java/com/ses/passwordkeeper/AccountDetailFragment.java
+++ app/src/main/java/com/ses/passwordkeeper/AccountDetailFragment.java
@@ -10,9 +10,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.EditText;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 public class AccountDetailFragment extends Fragment {
@@ -25,24 +23,6 @@ public class AccountDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //query database for accountId
-        try {
-
-            SQLiteOpenHelper accountDatabaseHelper = new AccountDatabaseHelper(inflater.getContext());
-            db = accountDatabaseHelper.getReadableDatabase();
-
-            cursor = db.query("Account",
-                        new String[]{"NAME"},
-                        "_id = ?",
-                        new String[] {Integer.toString(accountId - 1)},
-                        null, null, null);
-
-            cursor.moveToFirst();
-
-        } catch (SQLiteException e){
-            Toast toast = Toast.makeText(inflater.getContext(), "Database unavailable", Toast.LENGTH_SHORT);
-            toast.show();
-        }
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account_detail, container, false);
@@ -52,14 +32,30 @@ public class AccountDetailFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-
-        //testing
         View view = getView();
+
+        //query database for accountId
+        try {
+
+            SQLiteOpenHelper accountDatabaseHelper = new AccountDatabaseHelper(view.getContext());
+            db = accountDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("Account",
+                    new String[]{"NAME"},
+                    "_id = ?",
+                    new String[]{Integer.toString(accountId)},
+                    null, null, null);
+
+
+            cursor.moveToFirst();
+
+        } catch (SQLiteException e){
+            Toast toast = Toast.makeText(view.getContext(), "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
         EditText loginField = (EditText) view.findViewById(R.id.loginField);
         loginField.setText(cursor.getString(0));
-
-        //EditText passwordField = (EditText) view.findViewById(R.id.passwordField);
-        //passwordField.setText(Integer.toString(accountId));
 
     }
 
